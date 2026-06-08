7d9dc31e44cc32868214a1752e8f871d67dcddd2
Mateo Marcillaud <mateo.marcillaud@etu.utc.fr>
2026-06-08 11:41:26 -0300

    commenting on ENV variables. Removing hardcoding for encryption variables

--- API/auth.py
+++ API/auth.py
@@ -2,9 +2,12 @@ import jwt
 from datetime import datetime, timedelta, timezone
 from fastapi import HTTPException, Depends
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+import os
 
-SECRET_KEY = "my_super_secret_project_key_uff"
-ALGORITHM = "HS256"
+# did this to give the possibility to set a real secret key, for deployment and security reasons
+# allows to change the key if needed without changing the code, and also to just be able to launch the project without needing to set env variables
+SECRET_KEY = os.getenv("SECRET_KEY", "my_super_secret_project_key_uff")
+ALGORITHM = os.getenv("ALGORITHM", "HS256")
 
 
 security = HTTPBearer()
